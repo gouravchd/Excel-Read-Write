7eb3abf9bfb02df229e2296c9922d475cf4a003f
gouravchd <gourav0145@gmail.com>
2016-02-05 12:18:58 +0530

    excelRead.php
    Excel Read Code

---
+++ excelRead.php
@@ -0,0 +1,31 @@
+<?php
+// DOWNLOAD PHPEXCEL LATEST LIBRARY https://phpexcel.codeplex.com/ OR https://github.com/PHPOffice/PHPExcel
+require_once 'Classes/PHPExcel.php';
+require_once 'Classes/PHPExcel/IOFactory.php';
+
+$target_dir = "directory-url/";
+$target_file = $target_dir . basename('file-name.xlsx');
+
+	try {
+		$inputFileType = PHPExcel_IOFactory::identify($target_file);
+		$objReader = PHPExcel_IOFactory::createReader($inputFileType);
+		$objPHPExcel = $objReader->load($target_file);
+	} catch (Exception $e) {
+		die('Error loading file "' . pathinfo($target_file, PATHINFO_BASENAME) 
+		. '": ' . $e->getMessage());
+	}
+	
+	$sheet = $objPHPExcel->getSheet(0);
+	$highestRow = $sheet->getHighestRow();
+	$highestColumn = $sheet->getHighestColumn();
+
+//LOOP START FROM WHICH ROW	
+$colCell = '';
+for ($row = 2; $row <= $highestRow; $row++){
+    //EVERY ROW DATA
+    $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, TRUE,TRUE);
+    //COLLECT COLUMN CELL DATA ACCORDING TO USAGE
+    $colCell .= $rowData[$row]['B'];
+}
+echo  $colCell.'<br/>';
+?>
